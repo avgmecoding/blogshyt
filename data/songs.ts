121dd1cda13e93ee437bb28eb1ead7d9b1c7da9b
Rajshri Patil <rajshripatil19022006@gmail.com>
2026-01-03 23:24:12 +0530

    songs added on more route

---
+++ data/songs.ts
@@ -0,0 +1,9 @@
+export const SPOTIFY_DATA = {
+  prioritySong: {
+    title: "Ishq Bulaava",
+    artist: "Vishal-Shekhar, Sonam Puri, Shipra Goyal",
+    cover: "/song-cover.png", 
+    link: "https://open.spotify.com/track/1fkjRQA8wXPPyxqYLbxuqy?si=51b2a49b87a543cf", 
+  },
+  playlistLink: "https://open.spotify.com/playlist/7kSF9mBC3F6ay8QW8P4JkO?si=4ddac1a5619c422e",
+};
